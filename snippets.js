600a57608ad786ae58e8dca72037fcc0cd4d37ad
Kevin Le <kevinle108@gmail.com>
2023-02-22 13:26:58 -0500

    refactored

--- snippets.js
+++ snippets.js
@@ -1,17 +1,16 @@
+function get_home_facts_info(info) {
+    // Examples
+    // 'Price/Sq.Ft.'
+    // 'Year Built'
+    let ele = Array.from(document.querySelectorAll('span')).find(el => el.textContent === info);
+    return ele.nextSibling.innerText
+}
+
 const address = document.title.split('|')[0]
 const price = document.querySelector("div[data-rf-test-id=abp-price]").innerText.split('\n')[0]
 const sq_ft = document.querySelector("div[data-rf-test-id=abp-sqFt]").innerText.split('\n')[0]
 const bed = document.querySelector("div[data-rf-test-id=abp-beds]").innerText.split('\n')[0]
 const bath = document.querySelector("div[data-rf-test-id=abp-baths]").innerText.split('\n')[0]
-
-function get_home_facts_info(info) {
-    // Examples
-    // 'Price/Sq.Ft.'
-    // 'Year Built'
-    let data = Array.from(document.querySelectorAll('span')).find(el => el.textContent === info);
-    data = data.nextSibling.innerText
-    return data
-}
 
 const price_per_sqft = get_home_facts_info('Price/Sq.Ft.');
 const year_built = get_home_facts_info('Year Built');
