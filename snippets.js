569fb563d5979d3affb2639453b23137572fd366
Kevin Le <kevinle108@gmail.com>
2023-02-23 01:42:27 -0500

    working

--- snippets.js
+++ snippets.js
@@ -1,47 +1,32 @@
 // house_hoa: https://www.redfin.com/KY/Louisville/3901-Valley-Station-Rd-40272/home/84172457
 // house_no_hoa: https://www.redfin.com/KY/Louisville/8110-Kellerman-Rd-40219/home/71182885
 
-function get_home_facts_info(info) {
-    // Examples
-    // 'Price/Sq.Ft.'
-    // 'Year Built'
-    let ele = Array.from(document.querySelectorAll('span')).find(el => el.textContent === info);
-    let data = ''
-
+function get_value_sib(feature) {
+    const xpath = `//span[contains(text(),'${feature}')]//following-sibling::span`;
+    const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
     try {
-        data = ele.nextSibling.innerText
-    } catch (TypeError) {
-        // if (info == 'HOA Dues') data = 'none'
-    }
-    return data    
+        return result.singleNodeValue.textContent
+    } catch (error) {
+        return null
+    } 
 }
-
-function has_feature(feature) {
-    const el = Array.from(document.querySelectorAll('span'))
-                    .find(el => el.textContent === feature);
-    if (el) return true
-    else return false
-}
-
-function get_feature_child(feature) {
-    const el = Array.from(document.querySelectorAll('span'))
-                    .find(el => el.textContent.includes(feature));
-    return el.textContent.replace(feature, '')
-}
-
 
 function get_value(feature) {
     const xpath = `//span[contains(text(),'${feature}')]/span`;
     const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
-    return result.singleNodeValue.textContent    
+    try {
+        return result.singleNodeValue.textContent
+    } catch (error) {
+        return null
+    } 
 }
 
-const url = window.location.href
-const address = document.title.split('|')[0]
-const price = document.querySelector("div[data-rf-test-id=abp-price]").innerText.split('\n')[0]
-const sq_ft = document.querySelector("div[data-rf-test-id=abp-sqFt]").innerText.split('\n')[0]
-const bed = document.querySelector("div[data-rf-test-id=abp-beds]").innerText.split('\n')[0]
-const bath = document.querySelector("div[data-rf-test-id=abp-baths]").innerText.split('\n')[0]
+// const url = window.location.href
+// const address = document.title.split('|')[0]
+// const price = document.querySelector("div[data-rf-test-id=abp-price]").innerText.split('\n')[0]
+// const sq_ft = document.querySelector("div[data-rf-test-id=abp-sqFt]").innerText.split('\n')[0]
+// const bed = document.querySelector("div[data-rf-test-id=abp-beds]").innerText.split('\n')[0]
+// const bath = document.querySelector("div[data-rf-test-id=abp-baths]").innerText.split('\n')[0]
 
 
 
@@ -57,10 +42,12 @@ let info = {
     address: document.title.split('|')[0],
     price: document.querySelector("div[data-rf-test-id=abp-price]").innerText.split('\n')[0],
     sq_ft: document.querySelector("div[data-rf-test-id=abp-sqFt]").innerText.split('\n')[0],
-    bed: document.querySelector("div[data-rf-test-id=abp-beds]").innerText.split('\n')[0],
-    bath: document.querySelector("div[data-rf-test-id=abp-baths]").innerText.split('\n')[0],
-    age: get_value('Age: '),
+    price_per_sqft: get_value_sib('Price/Sq.Ft.'),
+    bed_bath: document.querySelector("div[data-rf-test-id=abp-beds]").innerText.split('\n')[0] + '_' + document.querySelector("div[data-rf-test-id=abp-baths]").innerText.split('\n')[0],
     year_built: get_value('Year Built: '),
+    age: get_value('Age: '),
+    lot_size: get_value_sib('Lot Size'),
+    property_type: get_value_sib('Property Type'),
     sqft_finished: get_value('Sq. Ft. (Total Finished): '),
     sqft_unfinished: get_value('Sq. Ft. (Total Unfinished): '),
     acres: get_value('Acres: ')
